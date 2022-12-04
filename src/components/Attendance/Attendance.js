c2715f7237e5b86d45969a2f237b713b0d21c63a
dmshikder <dmshikder@gmail.com>
2022-12-04 21:54:29 +0600

    attendance page responsive done

--- src/components/Attendance/Attendance.js
+++ src/components/Attendance/Attendance.js
@@ -18,16 +18,16 @@ const Attendance = () => {
   }, []);
   return (
     <div>
-      <div className="flex flex-col items-center gap-4 justify-center mt-10 ">
-        <Logo />
-        <h3 className="text-3xl font-semibold w-96 h-72 bg-blueEyes text-white rounded pt-4 ">
+      <div className="flex flex-col items-center gap-4 justify-center mt-10 md:flex-row md:gap-6 md:-ml-36">
+       <div className="md:-mt-28"> <Logo /></div>
+        <h3 className="text-3xl font-semibold w-96 h-72 bg-blueEyes text-white rounded pt-4 md:mt-10 md:h-16 md:pt-3">
           Attendance information
         </h3>
       </div>
 
       <table class=" flex flex-col items-center gap-6 ">
         <thead>
-          <tr className="flex flex-row gap-10 mt-6 bg-red-300 px-6 rounded">
+          <tr className="flex flex-row gap-10 mt-6 bg-red-300 px-6 rounded md:w-640 md:items-center md:justify-evenly md:py-4">
             <th>ID</th>
             <th>Name</th>
             <th>Position</th>
@@ -38,10 +38,10 @@ const Attendance = () => {
           {
             Object.keys(users).map((key,i)=>{
               return(
-                <tr className="flex flex-row gap-10 items-center bg-green-300 px-6 py-4 " key={i}>
+                <tr className="flex flex-row gap-10 items-center bg-green-300 px-6 py-4 md:w-640 md:items-center md:justify-evenly md:rounded border-2 " key={i}>
                   <td>{users[key].id}</td>
-                  <td className="w-16">{users[key].name}</td>
-                  <td className="w-16">{users[key].position}</td>
+                  <td className="w-16 md:w-28 text-left">{users[key].name}</td>
+                  <td className="w-16 md:w-24">{users[key].position}</td>
                   <td>{users[key].attendance["2022-11-02"].status}</td>
                 </tr>
               )
